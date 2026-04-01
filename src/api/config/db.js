118b78b10dc45766a6bb567939d2f0d54a1e09cb
Hemant Singh <honeyaps12345@gmail.com>
2026-04-01 22:00:17 +0530

    api testing goingon

--- src/api/config/db.js
+++ src/api/config/db.js
@@ -4,13 +4,9 @@ export const dbconnection = async () => {
  const MONGO_URL =
 "mongodb://honeyaps12345_db_user:Honey2100@ac-8kb6asg-shard-00-00.cqubytl.mongodb.net:27017,ac-8kb6asg-shard-00-01.cqubytl.mongodb.net:27017,ac-8kb6asg-shard-00-02.cqubytl.mongodb.net:27017/?ssl=true&replicaSet=atlas-7755o7-shard-0&authSource=admin&retryWrites=true&w=majority";
 
-  const options = {
-    serverSelectionTimeoutMS: 20000,
-    connectTimeoutMS: 60000,
-  };
 
   try {
-    await mongoose.connect(MONGO_URL, options);
+    await mongoose.connect(MONGO_URL);
 
     console.log("MongoDB Connected Successfully 🚀");
 
